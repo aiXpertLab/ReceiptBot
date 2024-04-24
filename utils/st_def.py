3599b7f7d676b525e22557aecf13b63f71ec3aca
aiXpertLab <aiXpertLab@gmail.com>
2024-04-24 10:46:53 -0400

    4

--- utils/st_def.py
+++ utils/st_def.py
@@ -4,7 +4,7 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 def st_sidebar():
     with st.sidebar:
         # store_link = st.text_input("Enter Your Store URL:",   value="http://hypech.com/StoreSpark", disabled=True, key="store_link")
-        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+        openai_api_key = st.text_input("OpenAI API Key (gpt-4)", key="chatbot_api_key", type="password")
         st.write("[Get an OpenAI API key](https://platform.openai.com/account/api-keys)")
         add_vertical_space(2)
         st.write('Made with ❤️ by [aiXpertLab](https://hypech.com)')
